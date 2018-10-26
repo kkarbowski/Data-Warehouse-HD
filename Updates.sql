62ef495c435a039d4a0e5c8000aae65cd3860a5e
Korneliusz Karbowski <karbowski.korneliusz@gmail.com>
2018-10-26 22:38:11 +0200

    first commit

---
+++ Updates.sql
@@ -0,0 +1,13 @@
+DECLARE @Narzut FLOAT
+SET @Narzut = 1.07
+
+UPDATE c SET c.CenaNettoAktualna = pz.CenaNetto*@Narzut
+FROM dbo.Czesci c, dbo.PozycjaZamowienia pz, dbo.Zamowienia z
+WHERE c.CzescID = pz.FK_Czesc AND
+pz.FK_Zamowienie = z.ZamowienieID AND
+z.DataRealizacjiZamowienia = (
+	SELECT MAX(DataRealizacjiZamowienia) FROM dbo.Zamowienia WHERE ZamowienieID = pz.FK_Zamowienie
+)
+
+UPDATE dbo.Czesci SET StanMagazynowy = (RAND()*20) + 5
+
